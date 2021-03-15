f336bef38e616a53fb423b44f3615660cb6099e3
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-03-15 15:52:42 +0100

    drop menu funzionante
    to do: CSS bello

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+var menuVoices = $(".voice");
 
+menuVoices.each(
+  function (){
+    $(this).hover(
+      function (){
+        var dropMenu = $(this).next();
+
+        dropMenu.addClass("active");
+
+      },
+      function (){
+        var dropMenu = $(this).next();
+
+        dropMenu.removeClass("active");
+      }
+    );
+  }
+);
