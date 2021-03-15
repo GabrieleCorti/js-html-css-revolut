7780674bf234d064a986cc68f4f6a53854be0439
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-03-15 16:34:28 +0100

    click funzionante

--- js/script.js
+++ js/script.js
@@ -1,19 +1,47 @@
 var menuVoices = $(".menu_voice");
 
-menuVoices.each(
+// menuVoices.each(
+//   function (){
+//     $(this).hover(
+//       function (){
+//         var dropMenu = $(this).children(".dropo_down_menu");
+//
+//         dropMenu.addClass("active");
+//
+//       },
+//       function (){
+//         var dropMenu = $(this).children(".dropo_down_menu");
+//
+//         dropMenu.removeClass("active");
+//       }
+//     );
+//   }
+// );
+
+menuVoices.hover(
   function (){
-    $(this).hover(
-      function (){
-        var dropMenu = $(this).children(".dropo_down_menu");
+    var dropMenu = $(this).children(".dropo_down_menu");
 
-        dropMenu.addClass("active");
+      dropMenu.addClass("active");
 
-      },
-      function (){
-        var dropMenu = $(this).children(".dropo_down_menu");
+    },
+  function (){
+    var dropMenu = $(this).children(".dropo_down_menu");
 
-        dropMenu.removeClass("active");
-      }
-    );
+    dropMenu.removeClass("active");
+    }
+);
+
+////quando clicco aggiungo una classe active
+
+menuVoices.click(
+  function (){
+    var dropMenu = $(this).children(".dropo_down_menu");
+
+    if ( !dropMenu.hasClass("active_click") ){
+      dropMenu.addClass("active_click");
+    } else {
+      dropMenu.removeClass("active_click");
+    }
   }
 );
