83253e8c144a6e00d0481ff1341627cdd486a5f5
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-03-15 16:02:28 +0100

    fixato l'hover

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-var menuVoices = $(".voice");
+var menuVoices = $(".menu_voice");
 
 menuVoices.each(
   function (){
     $(this).hover(
       function (){
-        var dropMenu = $(this).next();
+        var dropMenu = $(this).children(".dropo_down_menu");
 
         dropMenu.addClass("active");
 
       },
       function (){
-        var dropMenu = $(this).next();
+        var dropMenu = $(this).children(".dropo_down_menu");
 
         dropMenu.removeClass("active");
       }
